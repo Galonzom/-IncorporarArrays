f80e50ad68b5e18d98889bbc83e4d03de7ee8a26
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2022-08-14 18:03:38 -0400

    agregando clase, objeto y array

---
+++ js/form.js
@@ -0,0 +1,29 @@
+function capturar() {
+  function Persona(nombre, direccion, telefono, numeroident, email) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono
+    this.numeroident = numeroident;
+    this.email = email;
+  }
+  let nombreCapturar = document.getElementById("nombre").value;
+  let direccionCapturar = document.getElementById("direccion").value;
+  let telefonoCapturar = document.getElementById("telefono").value;
+  let numeroidentCapturar = document.getElementById("numeroident").value;
+  let emailCapturar = document.getElementById("email").value;
+
+  sujetoNuevo = new Persona(
+    nombreCapturar,
+    direccionCapturar,
+    telefonoCapturar,
+    numeroidentCapturar,
+    emailCapturar
+  );//crea el objeto
+  console.log(sujetoNuevo);
+  agregar();
+}
+let baseClientes = [];
+function agregar() {
+  baseClientes.push(sujetoNuevo);//agrega el objeto al Array
+  console.log(baseClientes);
+}
